551f13a7d1b0945e28377106ef6b4ced1aae18a1
ChrisWen960216 <chriswen96@163.com>
2018-10-29 22:30:18 +0800

    Add basic observable function

--- index/index.js
+++ index/index.js
@@ -8,5 +8,15 @@ import 'rxjs/add/observable/of';
  *  如何响应式事件，这是订阅者的责任，是subscribe来决定
  */
 
-const source$ = Observable.of(1, 2, 3);
-source$.subscribe(console.log);
+const onSubscribe = (observer) => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+};
+
+const source$ = new Observable(onSubscribe);
+const theObserver = {
+  next: item => console.log(item),
+};
+
+source$.subscribe(theObserver);
