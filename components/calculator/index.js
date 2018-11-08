92cbac464fb9877f228f4d781dd537e9d83e1fd6
ChrisWen <957638221@qq.com>
2018-11-08 10:51:56 +0800

    Add basic calculator

---
+++ components/calculator/index.js
@@ -0,0 +1,9 @@
+
+
+const $weightInput = document.getElementsByClassName('weight')[0];
+const $heightInput = document.getElementsByClassName('height')[0];
+const $calBtn = document.getElementsByClassName('cal-btn')[0];
+const $bmiContent = document.getElementsByClassName('bmi-content')[0];
+
+const weightValue = $weightInput.value;
+const heightValue = $heightInput.value;
--- components/drag/index.js
+++ components/drag/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 
 import { fromEvent } from 'rxjs';
-import { takeUntil, map, concatAll } from 'rxjs/operators';
+import { takeUntil, map, concatMap } from 'rxjs/operators';
 
 const $box = document.getElementsByClassName('root')[0];
 const { body: $body } = document;
@@ -12,14 +12,14 @@ const onMouseMove$ = fromEvent($body, 'mousemove');
 
 // const interval$ = interval(1000).pipe(takeUntil(onMouseUp$)).subscribe(console.log);
 
-onMouseDown$.pipe(
-  map(() => onMouseMove$.pipe(takeUntil(onMouseUp$))),
-  concatAll(),
-  map(m => ({
-    x: m.clientX,
-    y: m.clientY,
-  })),
-)
+onMouseDown$
+  .pipe(
+    concatMap(() => onMouseMove$.pipe(takeUntil(onMouseUp$))),
+    map(m => ({
+      x: m.clientX,
+      y: m.clientY,
+    })),
+  )
   .subscribe((position) => {
     $box.style.left = `${position.x}px`;
     $box.style.top = `${position.y}px`;
