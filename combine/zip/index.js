7fbb1d263e720158fb6c10b896483a73b3e22b27
ChrisWen <957638221@qq.com>
2018-12-06 09:53:44 +0800

    Learn combineLatest and withLatestFrom

--- combine/zip/index.js
+++ combine/zip/index.js
@@ -1,11 +1,15 @@
 import { timer } from 'rxjs';
-import { take, combineLatest } from 'rxjs/operators';
+import {
+  take, map, combineLatest, withLatestFrom,
+} from 'rxjs/operators';
 
-const sourceA$ = timer(0, 500).pipe(take(3));
-const sourceB$ = timer(500, 500).pipe(take(5));
+const sourceA$ = timer(0, 1500).pipe(map(number => number * 100), take(3));
+const sourceB$ = timer(0, 500).pipe(take(5));
 
 const project = (a, b) => `Received ${a} and ${b}`;
 
 const combineSource$ = sourceA$.pipe(combineLatest(sourceB$, project));
+const withLatestFrom$ = sourceA$.pipe(withLatestFrom(sourceB$));
 
 combineSource$.subscribe(console.log);
+withLatestFrom$.subscribe(console.log);
