9f3967fd1e7d6114d85f1aaffe2516461213f454
ChrisWen <chriswen96@163.com>
2019-12-03 14:53:56 +0800

    Init project

--- components/calculator/index.js
+++
@@ -1,9 +0,0 @@
-
-
-const $weightInput = document.getElementsByClassName('weight')[0];
-const $heightInput = document.getElementsByClassName('height')[0];
-const $calBtn = document.getElementsByClassName('cal-btn')[0];
-const $bmiContent = document.getElementsByClassName('bmi-content')[0];
-
-const weightValue = $weightInput.value;
-const heightValue = $heightInput.value;
--- operations/take/index.js
+++
@@ -1,6 +0,0 @@
-import { fromEvent, interval } from 'rxjs';
-import { defaultIfEmpty, takeUntil } from 'rxjs/operators';
-
-const debounceBtnEle = document.getElementsByClassName('debounce-btn')[0];
-const debounceBtn$ = fromEvent(debounceBtnEle, 'click').pipe(takeUntil(interval(5000)), defaultIfEmpty('No data!'));
-debounceBtn$.subscribe(console.log);
